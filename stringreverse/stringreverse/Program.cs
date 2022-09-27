cccda06a5728bb3201222c7e119d9f9b93c8f9f4
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:54:22 +0530

    Add files via upload

---
+++ stringreverse/stringreverse/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace stringreverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int l = 0;
+            
+            Console.WriteLine("input the string");
+            str = Console.ReadLine();
+
+            l = str.Length - 1;
+            Console.WriteLine("the characters of the string in reverse");
+            while(l>=0)
+            {
+                Console.WriteLine("{0}", str[l]);
+                l--;
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
