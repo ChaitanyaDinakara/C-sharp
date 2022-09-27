4ab45feb0859c0db762cf053ece2c0a2e21eb0e5
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 16:02:05 +0530

    Add files via upload

---
+++ Genericclass26/Genericclass26/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Genericclass26
+{
+    public class Program
+    {
+        public void GenericClassMethods<T1,T2,T3>(T1 id, T2 name, T3 status)
+        {
+            Console.WriteLine(id+ "," + name + "," +status);
+        }
+    
+        
+        public static void Main(string[] args)
+        {
+                Program p = new Program();
+            p.GenericClassMethods<int, string, char>(12, "Chaithu", 'P');
+            p.GenericClassMethods<int, string, char>(14, "Prasanth", 'N');
+            p.GenericClassMethods<int, string, char>(12, "Priya", 'P');
+            p.GenericClassMethods<int, string, char>(12, "Preethi", 'N');
+            p.GenericClassMethods<int, string, char>(12, "Gomathi", 'P');
+
+        }
+    }
+}
