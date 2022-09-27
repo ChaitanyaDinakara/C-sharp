8fe02a64e7beb01d6d86d2b8d99261007486ff42
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:55:07 +0530

    Add files via upload

---
+++ replace/replace/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace replace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            char[] arr;
+            int i, l;
+            l = 0;
+            char ch;
+            Console.WriteLine("enter a string");
+            str= Console.ReadLine();
+            l = str.Length;
+            arr = str.ToCharArray(0,l);
+
+            for (i = 0; i < l; i++)
+            {
+                ch=arr[i];
+                if (Char.IsLower(ch))
+                    Console.Write(Char.ToUpper(ch));
+                else
+                    Console.Write(Char.ToLower(ch));
+            }
+            
+        }
+        
+    }
+}
