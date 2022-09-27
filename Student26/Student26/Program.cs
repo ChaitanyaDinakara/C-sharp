349b6aa6cbdaae71506a21fb3ccce6325941816d
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 16:01:22 +0530

    Add files via upload

---
+++ Student26/Student26/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Student26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> stu = new Dictionary<int, string>();
+            {
+                try
+                {
+                    stu.Add(1234, "David");
+                    stu.Add(2345, "Kevin");
+                    stu.Add(7896, "Mark");
+                    stu.Add(3456, "Gamora");
+                    stu.Add(3293, "Vishal");
+                    stu.Add(3263, "Chaithu");
+                    stu.Add(3263, "Chaithu");
+                    stu.Add(4567, "Kiran");
+
+                }
+                catch(ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                foreach(KeyValuePair<int,string> keyValuePair in stu)
+                {
+                    Console.WriteLine(keyValuePair.Key.ToString() + " " + keyValuePair.Value);
+                }
+            }
+        }
+    }
+}
