44c9979ff0cb5b8835ee26ce2d63d6d0efadd6ae
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:56:26 +0530

    Add files via upload

---
+++ exception23/exception23/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exception23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "3.5";
+                int a = int.Parse(str);//format exception is thrown as the input is other than an integer
+            }
+            catch
+            {
+                Console.WriteLine("format exception");
+            }
+        }
+    }
+}
