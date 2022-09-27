d1052bccb34a9ab9beeed0f24d8241373aa40c0e
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:38:35 +0530

    Add files via upload

---
+++ reverse22/reverse22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace reverse22
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            char a, b, c;
+            Console.Write("input letter: ");
+            a = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("input letter: ");
+            b = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("input letter: ");
+            c = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0},{1},{2}", c, b, a);
+
+        }
+    }
+}
