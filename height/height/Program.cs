e67c49d7207093cad33f47c55acc3dbc20d655d6
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:47:32 +0530

    Add files via upload

---
+++ height/height/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace height
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float height;
+            Console.Write("enter the height (in cm)");
+            height = int.Parse(Console.ReadLine());
+
+
+            if (height < 135)
+                Console.WriteLine(" the person is short");
+            else if ((height > 135) && (height < 180))
+                Console.WriteLine("the person is medium height");
+            else if (height > 180)
+                    Console.WriteLine("the person is tall");
+            }
+        }
+    }
