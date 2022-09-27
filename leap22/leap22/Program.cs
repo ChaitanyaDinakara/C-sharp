2ec18dd2246409e9eebe685559078b7d68bc7854
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:43:23 +0530

    Add files via upload

---
+++ leap22/leap22/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace leap22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentyear;
+
+            Console.WriteLine("input the year");
+            currentyear=Convert.ToInt32(Console.ReadLine());
+
+            if ((currentyear % 400) == 0)
+                Console.WriteLine("{0} is a leap year", +currentyear);
+            else if ((currentyear % 100) == 0)
+                Console.WriteLine("{0} not a leap year", +currentyear);
+            else if ((currentyear % 4) == 0)
+                Console.WriteLine("{0} is a a leap year", +currentyear);
+            else
+                Console.WriteLine("{0} not a leap year", +currentyear);
+        }
+    }
+}
