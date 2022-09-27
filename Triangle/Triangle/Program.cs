61705dcfd5ee4a61fd36f65211da6cf3b49ff09d
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:50:54 +0530

    Add files via upload

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("enter the 1st side of triangle");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the 2nd side of trianle");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the third side of the triangle");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a == b && b == c)
+            {
+                Console.WriteLine("the triangle is equilateral ");
+            }
+            else if (a == b || b == c || c == a)
+            {
+                Console.WriteLine("the triangle is isosceles");
+            }
+            else
+            {
+                Console.WriteLine("The triangle is scalene ");
+            }
+        }
+    }
+}
