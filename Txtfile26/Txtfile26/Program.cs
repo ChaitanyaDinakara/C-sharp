cbd1c44a1b295658262abf9090a70163db07a8a0
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 16:00:30 +0530

    Add files via upload

---
+++ Txtfile26/Txtfile26/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Txtfile26
+{
+  public class FileHandling
+    {
+       public static void Main(string[] args)
+        {
+            StreamWriter streamWrite = new StreamWriter("C:\\Users\\chaitanya.dinakara\\OneDrive - Entain Group\\Documents\\assignment.txt");
+            streamWrite.WriteLine("Today is Monday");
+            streamWrite.WriteLine("First day of the week");
+            streamWrite.WriteLine("Excited for the week");
+            streamWrite.Close();
+
+           StreamWriter sw = File.AppendText("C:\\Users\\chaitanya.dinakara\\OneDrive - Entain Group\\Documents\\assignment.txt");
+            {
+                sw.WriteLine("Hi this is Chaitanya");
+                sw.WriteLine("Nice to meet u");
+            }
+            sw.Close();
+        }
+    }
+}
