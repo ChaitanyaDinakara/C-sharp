adb11066ed4e7f39e37593874a0c784bdb79d8f0
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:59:38 +0530

    Add files via upload

---
+++ bankacc23/bankacc23/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace bankacc23
+{
+    abstract class account
+    {
+        public abstract void display1();
+    }
+       class bankaccount : account
+        {
+            double aadhaarno;
+        public void link()
+            {
+                Console.WriteLine("enter the aadhar no");
+                aadhaarno= Convert.ToDouble(Console.ReadLine());
+            }
+            public override void display1()
+            {
+                Console.WriteLine("Account linked");
+            }
+           
+      
+                static void Main(string[] args)
+                {
+
+                    account acc = new bankaccount();
+                    acc.display1();
+                    acc.link();
+                    
+                }
+                    
+        }
+    }
+
