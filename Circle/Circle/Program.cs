c9cf8e16c2a3fd636a17157c2dd487e8ea08c573
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-27 15:39:57 +0530

    Add files via upload

---
+++ Circle/Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Circle
+{
+    internal class Program
+    {
+        class circle
+        {
+            double radius = 4,area,perimeter;
+            public void Area()
+            {
+                area = 3.14 * radius * radius;
+                perimeter = 2 * 3.14 * radius;
+                Console.WriteLine(area);
+                Console.WriteLine(perimeter);
+            }
+        }
+        static void Main(string[] args)
+        {
+            circle c= new circle();
+            c.Area();
+            
+        }
+    }
+}
