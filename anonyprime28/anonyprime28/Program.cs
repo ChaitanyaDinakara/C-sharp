c9d3f49348ba3b1fead13292462c20202dd6d36e
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-28 20:29:39 +0530

    Add files via upload
    anonymous method to check if the number is prime or not

---
+++ anonyprime28/anonyprime28/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace anonyprime28
+{
+    internal class Program
+    {
+        public delegate void cal(int a);
+        static void Main(string[] args)
+        {
+            
+            cal obj = delegate (int a)
+            {
+                int n = 0;
+                for(int i=1;i<=n;i++)
+                {
+                    if (n % i == 0)
+                    {
+                        a++;
+                    }
+
+                }
+                if(a==2)
+                {
+                    Console.WriteLine("it is a prime number");
+                }
+                else
+                {
+                    Console.WriteLine("not a prime number");
+                }
+
+            };
+            Console.WriteLine("enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            obj.Invoke(num);
+            
+        }
+    }
+}
