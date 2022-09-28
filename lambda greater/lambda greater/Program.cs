ca8094551d71abfea4b8c50244fa8be512dae691
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-28 16:26:47 +0530

    Add files via upload
    lambda expression to find the greater number

---
+++ lambda greater/lambda greater/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace lambda_greater
+{
+    internal class Program
+    {
+        public delegate int greater(int n1, int n2);
+        static void Main(string[] args)
+        {
+            greater f = (n1, n2) => n1 > n2 ? n1 : n2;
+            Console.WriteLine("enter the first number");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int n2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the greatest of the two numbers is :"+f.Invoke(n1, n2));
+        }
+    }
+}
